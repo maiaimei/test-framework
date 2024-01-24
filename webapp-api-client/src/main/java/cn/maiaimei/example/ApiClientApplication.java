7a06a00e5bc885114b6b0a868fca7d53b582390d
maiaimei <1211674185@qq.com>
2024-01-24 23:25:06 +0800

    feature: file upload

---
+++ webapp-api-client/src/main/java/cn/maiaimei/example/ApiClientApplication.java
@@ -0,0 +1,12 @@
+package cn.maiaimei.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiClientApplication.class, args);
+  }
+}
