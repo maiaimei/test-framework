0ee8405b7b1a27125398d3b010493ec5e22a512d
maiaimei <1211674185@qq.com>
2024-01-28 23:24:46 +0800

    feature: test python

---
+++ pytest.md
@@ -0,0 +1,18 @@
+The [`pytest`](https://docs.pytest.org/en/7.4.x/contents.html) framework makes it easy to write small, readable tests, and can scale to support complex functional testing for applications and libraries.
+
+`pytest` requires: Python 3.7+ or PyPy3.
+
+Install pytest
+
+```shell
+# -U 升级已安装的Python包
+pip install -U pytest
+```
+
+Check version
+
+```shell
+pytest --version
+```
+
+Create test files which filename start with "test_"
--- python-pytest-selenium/test_selenium_elements_locator_strategies.py
+++ python-pytest-selenium/test_selenium_elements_locator_strategies.py
@@ -13,7 +13,7 @@ from webdriver_helper import get_webdriver
 # 根据文本定位a标签
 # Locates anchor elements whose visible text matches the search value
 # 定位link text可视文本与搜索值完全匹配的锚元素
-def test_link_text():
+def test_find_element_by_link_text():
   with get_webdriver() as driver:
     driver.get("https://baidu.com")
     newsEl = driver.find_element(By.LINK_TEXT, "新闻")
@@ -25,7 +25,7 @@ def test_link_text():
 # Locates anchor elements whose visible text contains the search value. 
 # If multiple elements are matching, only the first one will be selected.
 # 定位link text可视文本部分与搜索值部分匹配的锚点元素。如果匹配多个元素，则只选择第一个元素。
-def test_partial_link_text():
+def test_find_element_by_partial_link_text():
   with get_webdriver() as driver:
     driver.get("https://baidu.com")
     newsEl = driver.find_element(By.PARTIAL_LINK_TEXT, "新")
@@ -34,7 +34,7 @@ def test_partial_link_text():
 
 
 # 根据标签定位任意元素
-def test_tag_name():
+def test_find_element_by_tag_name():
   with get_webdriver() as driver:
     driver.get("https://baidu.com")
     els = driver.find_elements(By.TAG_NAME, "a")
@@ -43,7 +43,7 @@ def test_tag_name():
 
 
 # 根据属性id定位任意元素
-def test_id():
+def test_find_element_by_id():
   with get_webdriver() as driver:
     driver.get("https://baidu.com")
     kwEl = driver.find_element(By.ID, "kw")
@@ -52,7 +52,7 @@ def test_id():
 
 
 # 根据属性name定位任意元素
-def test_name():
+def test_find_element_by_name():
   with get_webdriver() as driver:
     driver.get("https://baidu.com")
     kwEl = driver.find_element(By.NAME, "wd")
@@ -61,7 +61,7 @@ def test_name():
 
 
 # 根据属性class定位任意元素
-def test_class_name():
+def test_find_element_by_class_name():
   with get_webdriver() as driver:
     driver.get("https://baidu.com")
     el = driver.find_element(By.CLASS_NAME, "hot-refresh-text")
@@ -71,7 +71,7 @@ def test_class_name():
 
 
 # 通用定位策略：By.CSS_SELECTOR定位任意元素
-def test_class_selector():
+def test_find_element_by_class_selector():
   with get_webdriver() as driver:
     driver.get("https://baidu.com")
     kwEl = driver.find_element(By.NAME, "wd")
@@ -83,11 +83,11 @@ def test_class_selector():
 
 
 # 通用定位策略：By.XPATH定位任意元素
-def test_xpath():
+def test_find_element_by_xpath():
   with get_webdriver() as driver:
     driver.get("https://baidu.com")
     time.sleep(5)
 
 
 if __name__ == '__main__':
-  test_xpath()
+  test_find_element_by_xpath()
